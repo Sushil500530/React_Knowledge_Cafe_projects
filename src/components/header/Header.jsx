d71652c1da57f4d5d527a2caa08725a39519bf36
Sushil97 <sushil500530@gmail.com>
2023-09-11 19:41:17 +0600

    useState and useEffect use to load data

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+                  <h1 className='text-4xl text-center mt-4 font-semibold'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
